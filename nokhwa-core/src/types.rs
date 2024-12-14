06599aa67ca2a4fbabc55f39823cb8785f33c21e
YannPro <ydubrana@jllspear.com>
2024-12-14 04:17:04 +0100

    fix mozjpeg

--- nokhwa-core/src/types.rs
+++ nokhwa-core/src/types.rs
@@ -1593,7 +1593,7 @@ pub fn mjpeg_to_rgb(data: &[u8], rgba: bool) -> Result<Vec<u8>, NokhwaError> {
         }
     };
 
-    let scanlines_res: Option<Vec<u8>> = jpeg_decompress.read_scanlines_flat();
+    let scanlines_res: Result<Vec<u8>> = jpeg_decompress.read_scanlines_flat();
     // assert!(jpeg_decompress.finish_decompress());
     if !jpeg_decompress.finish_decompress() {
         return Err(NokhwaError::ProcessFrameError {
